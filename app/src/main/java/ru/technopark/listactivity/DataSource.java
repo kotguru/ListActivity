d0247329a6c8a8ab23cc41e50e70d7f6b8ca2126
Konstantin <gurovkost@gmail.com>
2020-03-18 13:16:25 +0300

    Ready for check, no warnings (#2)

--- app/src/main/java/ru/technopark/listactivity/DataSource.java
+++ app/src/main/java/ru/technopark/listactivity/DataSource.java
@@ -5,13 +5,13 @@ import android.graphics.Color;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DataSource {
+class DataSource {
 
     private List<MyData> mData;
 
     private static DataSource sInstance;
 
-    public DataSource(){
+    private DataSource(){
         mData = new ArrayList<>();
 
         for(int i = 1; i <= 100; i++) {
@@ -19,25 +19,25 @@ public class DataSource {
         }
     }
 
-    public synchronized static DataSource getInstance(){
+    synchronized static DataSource getInstance(){
         if (sInstance == null){
             sInstance = new DataSource();
         }
         return sInstance;
     }
 
-    public List<MyData> getData() {
+    List<MyData> getData() {
         return  mData;
     }
 
-    public static void addData(List<MyData> data){ data.add(new MyData(data.size() + 1)); }
+    static void addData(List<MyData> data){ data.add(new MyData(data.size() + 1)); }
 
 
-    public static class  MyData{
+    static class  MyData{
         int mNumber;
         int mColor;
 
-        public MyData(int mNumber) {
+        MyData(int mNumber) {
             this.mNumber= mNumber;
             mColor = (mNumber % 2) == 1 ? Color.BLUE : Color.RED;
         }
